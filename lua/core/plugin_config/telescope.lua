dd35fb331c8b7bf68be368b331f83245622e79c4
jekiquin <jci.dev.0@gmail.com>
2023-11-25 16:41:12 -0800

    added telescope branches

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,6 +1,8 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function() 
   builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', '<Leader>gb', ':Telescope git_branches<CR>', {noremap = true, silent = true})
