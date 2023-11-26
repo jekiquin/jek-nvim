89b4a54849b62856d17ba04c864d8e635b9db827
jekiquin <jci.dev.0@gmail.com>
2023-11-26 12:56:11 -0800

    added lsp and keymapping

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -12,3 +12,11 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
+
+vim.keymap.set('n', '<C-s>', ':w<CR>', {noremap = true, silent = true})
+vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>', {noremap = true, silent = true})
+vim.keymap.set('v', '<C-s>', '<Esc>:w<CR>', {noremap = true, silent = true})
+vim.keymap.set('n', '<leader>sq', ':wq<CR>')
+vim.keymap.set('n', '<leader>qq', ':q<CR>')
+vim.keymap.set('n', '<leader>h', '^', {noremap = true})
+vim.keymap.set('n', '<leader>l', '$', {noremap = true})
--- lua/core/plugin_config/init.lua
+++ lua/core/plugin_config/init.lua
@@ -3,4 +3,7 @@ require("core.plugin_config.lualine")
 require("core.plugin_config.nvim-tree")
 require("core.plugin_config.telescope")
 require("core.plugin_config.treesitter")
+require("core.plugin_config.lsp")
+require("core.plugin_config.null-ls")
+require("core.plugin_config.prettier")
 
