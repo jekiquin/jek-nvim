9201a4cba98c98de306bd31fae881c9f48a7295f
jekiquin <jci.dev.0@gmail.com>
2023-11-26 18:16:10 -0800

    fixed prettier configuration

--- lua/core/plugin_config/prettier.lua
+++ lua/core/plugin_config/prettier.lua
@@ -16,19 +16,6 @@ prettier.setup({
     "typescriptreact",
     "yaml",
   },
-   ["null-ls"] = {
-    condition = function()
-      return prettier.config_exists({
-        -- if `false`, skips checking `package.json` for `"prettier"` key
-        check_package_json = true,
-      })
-    end,
-    runtime_condition = function(params)
-      -- return false to skip running prettier
-      return true
-    end,
-    timeout = 5000,
-  },
   cli_options = {
     arrow_parens = "always",
     bracket_spacing = true,
