603969dbbd0c48341c66f910b9b9e6e202749aca
jekiquin <jci.dev.0@gmail.com>
2023-12-08 22:25:07 -0800

    add lsp indicator for bufferline

--- lua/core/plugin_config/bufferline.lua
+++ lua/core/plugin_config/bufferline.lua
@@ -8,6 +8,10 @@ vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
 require("bufferline").setup({
   options = {
     diagnostics = "nvim_lsp",
+    diagnostics_indicator = function(count, level)
+        local icon = level:match("error") and " " or ""
+        return " " .. icon .. count
+    end,
     mode = "buffers",
     offsets = {
       {
