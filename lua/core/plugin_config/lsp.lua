77cfd5df32a7ebe2641f613882259222950e7b3b
jekiquin <jci.dev.0@gmail.com>
2023-12-08 22:42:22 -0800

    added yamlls for lsp

--- lua/core/plugin_config/lsp.lua
+++ lua/core/plugin_config/lsp.lua
@@ -8,7 +8,7 @@ end)
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
-	ensure_installed = { "tsserver", "eslint", "pyright" },
+	ensure_installed = { "tsserver", "eslint", "pyright", "yamlls" },
 	handlers = {
 		lsp_zero.default_setup,
 	},
