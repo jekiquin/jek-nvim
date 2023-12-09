bf5a81877f98a31a3ed2f4b241387df7210389bb
jekiquin <jci.dev.0@gmail.com>
2023-12-08 19:12:01 -0800

    added tab and shift tab for tabbing through bufferline

--- lua/core/plugin_config/bufferline.lua
+++ lua/core/plugin_config/bufferline.lua
@@ -2,6 +2,8 @@ vim.opt.termguicolors = true
 
 vim.keymap.set("n", "<leader>bl", ":BufferLinePick<CR>")
 vim.keymap.set("n", "<leader>cbl", ":BufferLineCloseOthers<CR>")
+vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
 
 require("bufferline").setup({
   options = {
--- lua/core/plugin_config/fugitive.lua
+++ lua/core/plugin_config/fugitive.lua
@@ -1,2 +1,3 @@
 vim.keymap.set({'n', 'v'}, '<leader>gs', ':G<CR>', { noremap = true })
 vim.keymap.set({'n', 'v'}, '<leader>gdv', ':Gvdiffsplit!<CR>', { noremap = true })
+vim.keymap.set({'n', 'v'}, 'dg', ':diffget ', { noremap = true })
