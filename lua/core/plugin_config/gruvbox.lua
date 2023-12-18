04770d5dc67a2045c7d794be6c3cf5ff35469f47
jekiquin <jci.dev.0@gmail.com>
2023-12-18 00:33:33 -0800

    added some commented out code if we want transparent nvim

--- lua/core/plugin_config/gruvbox.lua
+++ lua/core/plugin_config/gruvbox.lua
@@ -1,2 +1,6 @@
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
+
+-- adding this in case we want to have transparent nvim
+vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
