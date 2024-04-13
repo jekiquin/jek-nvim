6da93b3569b05165c41c9e2fb58eef0ce13b8bdc
jekiquin <jci.dev.0@gmail.com>
2024-04-12 20:58:02 -0700

    replaced the shell to powershell if using windows

--- init.lua
+++ init.lua
@@ -4,3 +4,8 @@ require("core.plugin_config")
 
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+-- only change the shell when it's on windows
+if os.getenv('OS') == 'Windows_NT' then
+  vim.opt.shell = "powershell.exe"
+end
