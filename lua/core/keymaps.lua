57835dc0b046001e032a32ae0e44cdf59b341df8
jekiquin <jci.dev.0@gmail.com>
2024-06-12 10:42:56 -0700

    added more keymapping for motions, clipboard and quickfix

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -18,14 +18,27 @@ vim.cmd([[
 ]])
 
 vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<Esc><cmd>update<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-j>", "5j")
+vim.keymap.set("n", "<C-k>", "5k")
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
 vim.keymap.set("n", "<leader>qa", ":q<CR>")
 vim.keymap.set("n", "<leader>h", "^", { noremap = true })
 vim.keymap.set("n", "<leader>l", "$", { noremap = true })
 
 -- terminal on buffer
+-- these doesn't work well with powershell
 vim.keymap.set({ "n", "v" }, "<leader>tov", function()
 	vim.cmd("vsplit term://" .. vim.o.shell)
 end, { noremap = true })
 vim.keymap.set({ "n", "v" }, "<leader>toh", function()
 	vim.cmd("split term://" .. vim.o.shell)
 end, { noremap = true })
+
+vim.keymap.set({ "n", "v" }, "<leader>to", "<Esc><cmd>term<CR>", { noremap = true })
+
+-- quickfix
+vim.keymap.set("n", "<leader>qo", "<Esc><cmd>copen<CR>")
+vim.keymap.set("n", "<leader>qc", "<Esc><cmd>ccl<CR>")
+vim.keymap.set("n", "[q", "<Esc><cmd>cp<CR>")
+vim.keymap.set("n", "]q", "<Esc><cmd>cn<CR>")
