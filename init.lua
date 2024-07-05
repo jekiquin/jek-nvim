cc72cd30f8095e4dd02fb34ce016431bc15103b2
jekiquin <jci.dev.0@gmail.com>
2024-07-04 18:57:16 -0700

    removed providers

--- init.lua
+++ init.lua
@@ -5,6 +5,12 @@ require("core.plugin_config")
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+vim.g.loaded_python_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+-- vim.g.node_host_prog = vim.fn.system('nvm which current'):gsub('%s+', '') .. '/neovim-node-host'
+
 -- only change the shell when it's on windows
 if os.getenv('OS') == 'Windows_NT' then
   vim.opt.shell = "powershell.exe"
